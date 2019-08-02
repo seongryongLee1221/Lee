9844c8f70e799a4cafd17014c4457e639449143d
seongryongLee1221 <mnk1221@naver.com>
2019-08-02 14:35:59 +0900

    init

---
+++ Action.java
@@ -0,0 +1,11 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Action {
+
+	public ActionFoward execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	
+		
+}
